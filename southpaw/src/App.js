07312b69ac052936cc7a508d3f0ffb8aacb40ad4
Luis Robles <lfr4704@gmail.com>
2018-07-19 14:35:02 -0700

    navigation edits

--- southpaw/src/App.js
+++ southpaw/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './components/Navigation.js'
+import Navigation from './components/Navigation/Navigation.js'
+
 
 class App extends React.Component {
   render() {
--- southpaw/src/components/Navigation/Navigation.js
+++ southpaw/src/components/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Navigation.css'
 
 class Navigation extends React.Component {
 	render(){
