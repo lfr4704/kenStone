58d5dfedb3c8af8793af460607b7cb70cf70ad48
Luis Robles <lfr4704@gmail.com>
2018-07-19 23:18:59 -0700

    revised navigation

--- southpaw/src/components/Navigation/Navigation.js
+++ southpaw/src/components/Navigation/Navigation.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Navigation.css'
+import {Fabars} from 'react-icons/lib/fa';
 
 class Navigation extends React.Component {
 	render(){
 		return (
 			<nav>
+				<div className="toggle">
+					<i className="fa fa-bars" aria-hidden="true"></i>
+				</div>
 				<ul>
 					<li><a href="#">Home</a></li>
 					<li><a href="#">About</a></li>
